ffdc70adc3bc237b05489befabbb0f26aca601a3
Kai Pan <khaiphn41@gmail.com>
2021-05-27 21:19:00 -0700

    Wrote first test, this test checks if numbers that are multiples of 3 print "Fizz".

---
+++ test_fizzbuzz.py
@@ -0,0 +1,14 @@
+import fizzbuzz
+import pytest
+
+
+def test_fizz(capsys):
+    # Use for loop to test a few numbers that are multiples of 3
+    for i in [3, 6, 9]:
+        fizzbuzz.fizzbuzz(i)
+        # capture printed output on stdout to test
+        captured = capsys.readouterr()
+
+        # Assert test on captured output
+        # Note: print in python adds newline so check for it too
+        assert captured.out == "Fizz\n"
