2d12f7d13696aa20c4c526e2e779bbce1c33b028
Kai Pan <khaiphn41@gmail.com>
2021-05-27 23:11:04 -0700

    Wrote third test, checking that non leap years (non divisible by 4 or divisible by 4 and 100 but not 400) return False.

--- test_leap_year.py
+++ test_leap_year.py
@@ -5,3 +5,9 @@ import leap_year
 def test_leap_years():
     assert leap_year.is_leap_year(2016)
     assert leap_year.is_leap_year(2000)
+
+
+# Test non leap years
+def test_non_leap_years():
+    assert leap_year.is_leap_year(1999) is False
+    assert leap_year.is_leap_year(2100) is False
