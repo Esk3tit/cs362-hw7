44ae8f3c04dda510edcf65f84ef1af56f5444c48
Kai Pan <khaiphn41@gmail.com>
2021-05-27 21:46:27 -0700

    Implemented fizzbuzz which prints out 'Buzz' for multiples of 5 only

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -3,3 +3,7 @@ def fizzbuzz(num):
     # Fizz print (if multiple of 3)
     if num % 3 == 0:
         print("Fizz")
+
+    # Buzz print (if multiple of 5)
+    elif num % 5 == 0:
+        print("Buzz")
--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -13,9 +13,10 @@ def test_fizz(capsys):
         # Note: print in python adds newline so check for it too
         assert captured.out == "Fizz\n"
 
+
 def test_buzz(capsys):
     # Use for loop to test a few numbers that are multiples of 5
-    for i in [5, 10, 15]:
+    for i in [5, 10, 25]:
         fizzbuzz.fizzbuzz(i)
         # capture printed output on stdout to test
         captured = capsys.readouterr()
