e9ab8b3729cd426ad960b9c761b436350c45d19b
Kai Pan <khaiphn41@gmail.com>
2021-05-27 21:53:47 -0700

    Implemented fizzbuzz which prints 'FizzBuzz' whenever a number is a multiple of both 3 and 5.

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,7 +1,14 @@
 def fizzbuzz(num):
 
+    # FizzBuzz print (if multiple of 3 and 5)
+    # Check this first because we if do the others first then we would already
+    # print Fizz because the number is a multiple of 3 but we check it before
+    # also checking if it is a multiple of 5 for FizzBuzz
+    if num % 3 == 0 and num % 5 == 0:
+        print("FizzBuzz")
+
     # Fizz print (if multiple of 3)
-    if num % 3 == 0:
+    elif num % 3 == 0:
         print("Fizz")
 
     # Buzz print (if multiple of 5)
